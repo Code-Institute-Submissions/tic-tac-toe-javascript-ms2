7ea4a8f146a61d3fee3fa9f1e7e8749ee2f4814e
kaytee123 <denniseshun21@gmail.com>
2021-01-20 20:11:35 +0000

    creating 9 cells for the game

---
+++ script.js
@@ -0,0 +1,7 @@
+  
+$(document).ready(function() {
+
+  // initialize variables
+  // human = either x or o, whatever the individual picks
+  // ai = either x or o, whatever the player doesn't pick
+  var human, ai;
